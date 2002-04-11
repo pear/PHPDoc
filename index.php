ace3cf991a30c23f0822f8a52bc14a929a7ea163
Tim Gallagher <timmyg@php.net>
2002-04-11 16:03:08 +0000

    Too many changes to list, look at source.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHPDoc/trunk@77719 c90b9560-bf6c-de11-be94-00142212c4b1

--- index.php
+++ index.php
@@ -1,106 +1,128 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<?PHP
+$start = time();
 
-<html>
-<head>
-	<title>PHPDOC Version 1.0beta</title>
-</head>
+// 3/12/2002 - Tim Gallagher<timg@sunflowerroad.com> added the version checking
+// included below to deal with the different ways php sends variables into scripts.
+// see the file for more information.
+include ("versionSanity.php");
 
-<body>
-	<table width="560">
-		<tr>
-			<td align="left" valign="top">
-				<font face="Arial, Helvetica" size="2">
-					<h1>Welcome to PHPDoc!</h1>
-				
-					PHPDoc currently requires a late PHP4 version (4.0.3dev) to work.
-					Some earlier version like to crash with memory trouble. 
-					<p>
-					PHPDoc was programmed without	using PHP4 features that means 
-					it will run with version prior to	4.0.3dev on fine day. 
-					But again: my PHP3.0.15 crashed when it started to render the 
-					HTML documents :(.
-					<p>
-					I'll try to provide a solution as soon as possible.
-				</font>
-			</td>
-		<tr>
-			<td align="left" valign="top"><hr></td>
-		</tr>
-		<tr>
-			<td align="left" valign="top">
-				<pre>
-				<?php
-				$start = time();
-				
-				// WARNING: long runtimes! Make modifications 
-				// to the php[3].ini if neccessary. A P3-500 
-				// needs slightly more than 30 seconds to 
-				// document phpdoc itself.
-						
-				// Directory with include files
-				define("PHPDOC_INCLUDE_DIR", "c:/www/apache/doc/");
-				// Important: set this to the Linebreak sign of your system!
-				if (substr(php_uname(), 0, 7) == "Windows") $lnbreak = "\r\n";
-				else $lnbreak = "\n";
-				define("PHPDOC_LINEBREAK", $lnbreak);
+// WARNING: long runtimes! Make modifications 
+// to the php[3].ini if neccessary. A P3-500 
+// needs slightly more than 30 seconds to 
+// document phpdoc itself.
 
-				// Html tags allowed to be used in docs
-				define("PHPDOC_ALLOWEDHTMLTAGS", "<a>,<i>,<b>,<pre>,<ul>,<li>,<br>,<code>");
-		
-				// main PHPDoc Include File
-				include("./prepend.php");		
-		
-				$doc = new Phpdoc;
-				
-				// Sets the name of your application.
-				// The name of the application gets used in many default templates.
-				$doc->setApplication("PEAR Repository");
-				
-				// directory where your source files reside:
-				$doc->setSourceDirectory("c:/www/apache/form/");
-				
-				// save the generated docs here:
-				$doc->setTarget("c:/www/apache/doc/apidoc/");
-				
-				// use these templates:
-				$doc->setTemplateDirectory("c:/www/apache/doc/renderer/html/templates/");
-				
-				// source files have one of these suffixes:
-				$doc->setSourceFileSuffix( array ("php", "inc") );
-		
-				// parse and generate the xml files
-				$doc->parse();
-				
-				// turn xml in to html using templates
-				$doc->render();
-				
-				printf("%d seconds needed\n\n.", time() - $start);
-				?>
-				</pre>	
-			</td>
-		</tr>
-		<tr>
-			<td align="left" valign="top"><hr></td>
-		</tr>
-		<tr>
-			<td align="left" valign="top">
-				<font face="Arial, Helvetica" size="2">
-					<h2>Finished!</h2>
-					PHPDoc has finished his work. The generated XML and HTML files can be found in
-					the directory specified with setTarget() in the above code. This is per default "installationdir/apidoc/".
-					Within this directory is another directory named "keep/". It contains a stylesheet file and 
-					a frameset you can use to browse the HTML files. 
-					<p>
-					Don't be disappointed if PHPDoc makes documentation mistakes. Always remember it's currently
-					only grabbing not parsing. If PHPDoc will develop as a standard this will change as soon 
-					as possible. Please be patient and wait until "... later this year." ;-).
-					<p>
-					Have fun!
-					<p>
-					<a href="mailto:ulf.wendel@phpdoc.de">Ulf</a>
-				</font>
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+// 3/8/2002 Tim Gallagher - cleaned up the code on this page
+// by making the html on this page a function call.
+// this makes the indenting much cleaner.
+// new functions can be found at the bottom of prepend.php
+
+//SERVER_DOC_ROOT is defined in versionSanity.php
+
+// Directory with include files
+define("PHPDOC_INCLUDE_DIR", SERVER_DOC_ROOT . "/apps/PHPDoc/");
+
+// Html tags allowed to be used in doc tags
+define("PHPDOC_ALLOWEDHTMLTAGS", "<a>,<i>,<b>,<pre>,<ul>,<li>,<br>,<code>");
+
+// main PHPDoc Include File
+include("./prepend.php");		
+
+// look at the bottom of prepend.php for this function
+// this function also includes front-end.php
+mainPageHeader();
+
+// output buffering used to format the previously <pre>ed text on this page prettier.
+ob_start();
+$doc = new Phpdoc;
+
+// Sets the name of your application.
+// The name of the application gets used in many default templates.
+$doc->setApplication($_REQUEST['PHPDOC_appname']);
+
+// directory where your source files reside:
+$doc->setSourceDirectory(SERVER_DOC_ROOT . $_REQUEST['PHPDOC_sourcedir']);
+
+// save the generated docs here:
+$doc->setTarget(SERVER_DOC_ROOT . $_REQUEST['PHPDOC_targetdir']);
+
+$templateName = $_REQUEST['PHPDOC_selectedTemplate'];
+
+// use these templates:
+// this is the data sent in from the html form.
+$doc->setTemplateDirectory($PHPDOC_templates[$templateName]['path']);
+
+// source files have one of these suffixes:
+$doc->setSourceFileSuffix( array ("php", "inc") );
+
+/* ------------------------------------------------------------------
+// 3/2/2002 - Tim Gallagher<timg@sunflowerroad.com> changed this section to allow
+// for deleting files in the existing targetdir location
+// this should be considered a HUGE security risk... we're not
+// checking any file permissions to see what we're deleting.
+*/
+if ($_REQUEST['PHPDOC_deleteFilesOption'] =="ON") {
+    // uncomment the line below to turn file deleting on.
+    // if you do, you MUST be sure that only trusted people have access to the PHPDoc directory
+    // because a malicious user could send bogus data to the script and delete EVERYTHING on your webserver.
+    //include ("delete_files.php");
+}; // end if
+
+// parse and generate the xml files
+$doc->parse();
+
+/* ------------------------------------------------------------------
+3/1/2002 - Tim Gallagher<timg@sunflowerroad.com> changed this section (added it really) to
+automatically copy the phpdoc.css file (and others) to the destination
+directory automatically so it's always there.
+also added an auto copy for the frameset, dtd, and empty.html files.
+*/
+
+/* ------------------------------------------------------------------
+3/11/2002 - Tim Gallagher<timg@sunflowerroad.com> changed this section
+to use the template array as defined in /renderer/templates.php
+for the path in the following section
+*/ 
+
+// 3/11/2002 - Tim Gallagher<timg@sunflowerroad.com> added this note:
+// we do a str_replace here to keep all paths using the forward slash (which is compatible under
+// linux, unix, and windows - make sure apache is set up correctly under win32 - in other words
+// make sure apache is using forward slashes in the virtual directive (or in the document root directive)
+$fileToCopySrc = str_replace("\\","/", realpath($PHPDOC_templates[$templateName]['static_files_path']) . "/");
+$fileToCopyDst = str_replace("\\","/", realpath(SERVER_DOC_ROOT. $_REQUEST['PHPDOC_targetdir']) . "/");
+
+// 3/11/2002 - Tim Gallagher<timg@sunflowerroad.com> added this note:
+// we've already scanned the static file path in templates.php located in PHPDOC/renderer/
+// so we can simply set our $filesToCopy to the array defined in the $PHPDOC_templates variable
+$filesToCopy = $PHPDOC_templates[$templateName]['files'];
+echo "Copying files now...\n";
+for ($a = 0; $a < count($filesToCopy) ; $a++) {
+    //echo "Copying $fileToCopySrc$filesToCopy[$a].\n";
+    if (! (copy ($fileToCopySrc . $filesToCopy[$a], $fileToCopyDst . $filesToCopy[$a])) ) {
+        echo "An error occured trying to copy $fileToCopySrc$filesToCopy[$a].\n\n";
+    }; // end if
+}; // end for loop
+
+// ------------------------------------------------------------------
+
+// 3/11/2002 - Tim Gallagher<timg@sunflowerroad.com> moved the render method call 
+// and put it below the file copy section
+// so we can copy a dummy warnings html file to the destination, but have it overwritten
+// by the render method if warnings do exist.  I got really tired of having a 404 error on all my pages
+// when clicking on the "Report" link.
+
+// turn xml into html using templates
+$doc->render();
+
+echo time() - $start . " seconds needed.";
+$out = ob_get_contents();
+ob_end_clean();
+
+// display a link to view the documentation with.
+echo '<a href="' . $_REQUEST['PHPDOC_targetdir'] . '/">View your generated documentation here.</a><br>';
+
+echo nl2br($out);
+
+// look at the bottom of prepend.php for this function
+mainPageFooter();
+
+?>
